b946debd6d838e0f68a3eff19bfc85244ef9c8ba
Hwanho Jang <allmind75@gmail.com>
2018-05-07 11:49:43 +0900

    chap3

--- chap2/balance.js
+++ chap2/balance.js
@@ -1,5 +1,5 @@
 //geth에서 loadScript('script 경로'); 로 javascript로 작성한 메소드 사용 가능
-//loadScript('git/blockchain-basic/chap2/balances.js')
+//loadScript('git/blockchain-basic/chap2/balance.js')
 
 function checkAllBalances() {
   var i = 0;
